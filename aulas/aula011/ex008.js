e91f43aecec77cecdc9062ecb678165da516b2a9
João Victor Mian Valdomiro <joaovictormian@gmail.com>
2024-03-05 19:10:19 -0300

    ex

---
+++ aulas/aula011/ex008.js
@@ -0,0 +1,9 @@
+
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel} KM/H.`)
+if (vel > 60){
+    console.log("Você ultrapassou a velocidade permitida! MULTADO!")
+} /*else {
+    console.log("Continue assim!")
+}*/
+console.log("Dirija sempre usando o cinto de segurança!")
