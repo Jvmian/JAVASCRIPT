b0bd03813b10946f8b6d695715d97a05964056c2
João Victor Mian Valdomiro <joaovictormian@gmail.com>
2024-03-11 01:13:19 -0300

    finish day

---
+++ aulas/aula013/teste.js
@@ -0,0 +1,16 @@
+var c = 1
+while (c <= 20) {
+    console.log(`Eu quero ${c}`)
+    c++
+}
+var c = 1
+do {
+   console.log(`Passo ${c}`)
+   c++
+} while(c <= 7)
+/*
+console.log('Oi, tudo bem ? como tá ?')
+console.log('Oi, tudo bem ? como tá ?')
+console.log('Oi, tudo bem ? como tá ?')
+console.log('Oi, tudo bem ? como tá ?')
+*/
