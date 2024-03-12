6ad7ff9a3391abdee548f5f378e179a5d7b27e3f
João Victor Mian Valdomiro <joaovictormian@gmail.com>
2024-03-11 23:10:14 -0300

    ex

---
+++ aulas/aula014/testee.js
@@ -0,0 +1,13 @@
+console.log("Vai começar!")
+for(var d = 1; d <= 5; d++){
+    console.log(`É isso ${d}x`)
+}
+console.log("FIM!")
+
+/*
+var d = 1
+while (d <= 5) {
+    console.log(`É isso ${c}x`)
+    d++
+}
+*/
