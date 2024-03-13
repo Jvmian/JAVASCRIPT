82e3b210bb77b27e8e226ccd482d9bf6ce90aadc
João Victor Mian Valdomiro <joaovictormian@gmail.com>
2024-03-13 00:22:35 -0300

    comecando

---
+++ exercícios/ex003/modelo/script.js
@@ -0,0 +1,16 @@
+function calcular (){
+var inicio = window.document.getElementById('iinicio')
+var fim = window.document.getElementById('ifim')
+var contar = window.document.getElementById('icontar')
+var resultado = window.document.getElementById('result')
+
+resultado.innerHTML = `Vamos contar`
+if (inicio < 0){
+    window.alert('[ERRO] Coloque um início válido!')
+}
+for (inicio; fim; contar){
+    
+}
+
+
+}   
